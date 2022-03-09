faa6db239f692a9773a46ecac04fa7246386acbb
Leonard <lonardlator@yahoo.fr>
2022-03-09 11:10:53 +0100

    carousel improved

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,24 +11,25 @@ function Mycarousel ({myLoader}) {
       <div className="text-center mb-5">
        <h2 className="section-heading text-uppercase">Gallery</h2>
       </div>
-      <div className="container-fluid">
+      <div className="container">
 
-      <Carousel fade variant="dark">
+      <Carousel fade variant="dark" >
       {galleryLinks.map((haircut) => {
         return (
-          <Carousel.Item key={haircut.id}>
+          <Carousel.Item key={haircut.id} className="d-block"  >
            <Image
              loading="lazy"
              placeholder="blur" 
              blurDataURL={`/img/portfolio/t_${haircut.id}.jpg`}
              layout="responsive"
              loader={myLoader}
-             width={1000}
-             height={500}
-             className=""
+             width='100%'
+             height='100%'
+             className="img-fluid d-inline-block "
              src={`/img/portfolio/t_${haircut.id}.jpg`}
              alt="..."
-             objectFit="cover"
+             objectFit="contain"
+             objectPosition="50% 50%"
              sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
              srcSet="large.jpg 1024w, medium.jpg 640w,small.jpg 320w"
              />
